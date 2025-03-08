ff264e489b540a31003588277021c0449e06f153
af2233 <a.fadeev.101@gmail.com>
2025-03-08 15:49:58 +0300

    switched from drf-yasg to drf-spectacular

--- backend/django_app/django_app/settings.py
+++ backend/django_app/django_app/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'drf_yasg',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +138,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema', 
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'zonbirovanie',
+    'DESCRIPTION': 'IT project',
+    'VERSION': '0.4.2',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
