c4515d27dafd0e6cbabdceaeaa4550472f75b616
af2233 <a.fadeev.101@gmail.com>
2025-04-12 20:01:47 +0300

    switch to built-in user model

--- backend/django_app/users/admin.py
+++ backend/django_app/users/admin.py
@@ -1,8 +1,7 @@
-# users/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import MyUser
+# from .models import MyUser
 
 # Регистрируем модель в админке:
-admin.site.register(MyUser, UserAdmin)
+# admin.site.register(MyUser, UserAdmin)
