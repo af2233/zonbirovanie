c609e0379ec7a8ff233cf1a7ab39071958edbe56
CrazyCatGames <paveldomkin@gmail.com>
2025-05-22 12:01:40 +0300

    Added ML_service

--- ML/service/engine.py
+++ ML/service/engine.py
@@ -8,7 +8,7 @@ from PIL import Image
 from tqdm import tqdm
 from pathlib import Path
 
-from model import UNet, get_transforms
+from .models import UNet, get_transforms
 
 # гиперпараметры 
 SEED = 666
@@ -16,19 +16,18 @@ SEED = 666
 DATA_PATH = Path('photos')
 MODELS_PATH = Path('models')
 ACHIEVE_PATH = Path('input.zip')
-INPUT_PATH = DATA_PATH 
+INPUT_PATH = DATA_PATH
 RESULT_PATH = DATA_PATH / 'result'
-CHECKOPOINT = MODELS_PATH / 'model_43iou.pth'
+CHECKOPOINT = MODELS_PATH / 'models/model_7.pth'
 
-DROPOUT = 0.2 # параметр который нужен для создания модели
+DROPOUT = 0.2  # параметр который нужен для создания модели
 IMG_SIZE = 256
-BATCH_SIZE = 16 
+BATCH_SIZE = 16
 
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu' # на чем будет все будет работать, если нет nvidia & cuda не советую запускать
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'  # на чем будет все будет работать, если нет nvidia & cuda не советую запускать
 
 
 def set_seed(seed: int = SEED) -> None:
-
     random.seed(seed)
     torch.manual_seed(seed)
 
@@ -39,7 +38,6 @@ def set_seed(seed: int = SEED) -> None:
 
 def safe_extract(zip_path: Path,
                  extract_to: Path = INPUT_PATH) -> None:
-    
     if not os.path.exists(INPUT_PATH):
         os.mkdir(INPUT_PATH)
 
@@ -50,10 +48,10 @@ def safe_extract(zip_path: Path,
         total_size = sum(file.file_size for file in zip_ref.infolist())
         extracted_size = 0
         max_size = 1024 * 1024 * 1024  # 1 Gb (можно изменить)
-        
+
         if total_size > max_size:
             raise ValueError("Achieve too big")
-        
+
         for file in zip_ref.infolist():
             extracted_size += file.file_size
             if extracted_size > max_size:
@@ -65,16 +63,17 @@ def predict(model: torch.nn.Module,
             input_path: str = INPUT_PATH,
             result_path: str = RESULT_PATH,
             batch_size: int = BATCH_SIZE) -> None:
-    
-    X_trans, _ = get_transforms(IMG_SIZE) # получаем трансформации
-    input_images_paths = sorted(list(os.walk(input_path))[0][2]) # собираем названия всех изображений
+    X_trans, _ = get_transforms(IMG_SIZE)  # получаем трансформации
+    input_images_paths = sorted(
+        list(os.walk(input_path))[0][2])  # собираем названия всех изображений
 
     for batch_start in tqdm(range(0, max(len(input_images_paths), batch_size), batch_size)):
         # изображения объеднияем в батч, так как модель будет обрабатывать небольшой батч и 1 изображение одинаково по скорости (но не по памяти)
         batch_end = min(batch_start + batch_size, len(input_images_paths))
         batch_files = input_images_paths[batch_start:batch_end]
-        print(f"Processing batch {batch_start//batch_size + 1}: files {batch_start}-{batch_end-1}")
-        
+        print(
+            f"Processing batch {batch_start // batch_size + 1}: files {batch_start}-{batch_end - 1}")
+
         batch_images = []
         for img_path in batch_files:
             try:
@@ -86,12 +85,12 @@ def predict(model: torch.nn.Module,
             except Exception as e:
                 print(f"Error processing {img_path}: {str(e)}")
                 continue
-        
+
         batch_images = torch.stack(batch_images).to(DEVICE)
         # размерность batch_images - (5, 3, 256, 256) - то есть 5 изображений
         with torch.inference_mode():
-            pred = model(batch_images) # получаем маски, разменость которых так же (5, 3, 256, 256)
-    
+            pred = model(
+                batch_images)  # получаем маски, разменость которых так же (5, 3, 256, 256)
 
         for img_path, pred_tensor in zip(batch_files, pred):
             base_name = os.path.splitext(os.path.basename(img_path))[0]
@@ -101,7 +100,6 @@ def predict(model: torch.nn.Module,
 
 
 def main():
-
     set_seed()
     # создаем модель и загружаем веса
     unet_model = UNet(dropout=DROPOUT).to(DEVICE)
@@ -109,8 +107,6 @@ def main():
     # распаковываем и делаем маски
     safe_extract(zip_path=ACHIEVE_PATH, extract_to=INPUT_PATH)
     predict(model=unet_model, input_path="photos/input")
-
-        
 
 
 if __name__ == '__main__':
