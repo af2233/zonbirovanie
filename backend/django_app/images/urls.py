1fd2bca1e20705b3ccbf2dcc760954b37d650e3f
af2233 <a.fadeev.101@gmail.com>
2025-05-25 20:41:38 +0300

    fix api

--- backend/django_app/images/urls.py
+++ backend/django_app/images/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,4 @@ urlpatterns = [
     path('process/<int:archive_id>/', views.process_images, name='process_images'),
     path('download/<int:archive_id>/', views.download_processed_archive, name='download_processed_archive'),
     path('receive/<int:archive_id>/', views.receive_processed_archive, name='receive_processed_archive'),
-] 
+]
